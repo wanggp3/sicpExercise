2ddc2d01254ef71d48e3875a4a668ad02a060a07
wanggp3 <wanggp3@mail2.sysu.edu.cn>
2015-04-14 13:58:24 +0800

    add implement of derivation

---
+++ chap2/derivative.scm
@@ -0,0 +1,65 @@
+
+;;;	(variable? e)
+;;; (same-variable? v1 v2)
+;;; (sum? e)
+;;; (addend e)   // addend of the sum e
+;;; (augend e)   // augend of the sum e
+;;; (make-sum a1 a2)
+;;; (product? e)
+;;; (multiplier e)
+;;; (multiplicand e)
+;;; (make-product a1 a2)
+
+;;;  cal the dericative of the expression with format of [ a op b ]
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+	(and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+;;;(define (make-sum a1 a2) (list '+ a1 a2))
+(define (=number? exp val)
+	(and (number? exp) (= exp val)))
+
+(define (make-sum a1 a2)
+	(cond ((=number? a1 0) a2)
+		  ((=number? a2 0) a1)
+		  ((and (number? a1) (number? a2)) (+ a1 a2))
+		  (else (list '+ a1 a2))))
+
+;;;(define (make-product a1 a2) (list '* a1 a2))
+(define (make-product a1 a2) 
+	(cond ((or (=number? a1 0) (=number? a2 0)) 0)
+		  ((=number? a1 1) a2)
+		  ((=number? a2 1) a1)
+		  ((and (number? a1) (number? a2)) (* a1 a2))
+		  (else (list '* a1 a2))))
+
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+
+(define (addend x) (cadr x))
+
+(define (augend x) (caddr x))
+
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier x) (cadr x))
+
+(define (multiplicand x) (caddr x))
+
+;;;;;;
+
+
+(define (deriv exp var)
+	(cond ((number? exp) 0)
+		  ((variable? exp) 
+					(if (same-variable? exp var) 1 0))
+		  ((sum? exp) (make-sum (deriv (addend exp) var)
+							    (deriv (augend exp) var)))
+		  ((product? exp)
+					(make-sum 
+						(make-product (multiplier exp)
+									  (deriv (multiplicand exp) var))
+						(make-product (multiplicand exp)
+									  (deriv (multiplier exp) var))))
+		  (else (error "Unknown exp " exp))))
