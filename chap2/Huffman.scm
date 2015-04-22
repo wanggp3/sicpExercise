9db93163e3b4883825beb72eddc46a07daf7fd78
wanggp3 <wanggp3@mail2.sysu.edu.cn>
2015-04-23 00:21:23 +0800

    add Huffman-tree && 2.67

---
+++ chap2/Huffman.scm
@@ -0,0 +1,55 @@
+
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+(define (leaf? object) (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+	(if (leaf? tree)
+		(list (symbol-leaf tree))
+		(caddr tree)))
+(define (weight tree)
+	(if (leaf? tree)
+		(weight-leaf tree)
+		(cadddr tree)))
+
+(define (make-code-tree left right)
+	(list left
+		  right
+		  (append (symbols left) (symbols right))
+		  (+ (weight left) (weight right))))
+
+(define (choose-branch bit cur-node)
+	(cond ((= bit 0) (left-branch cur-node))
+	      ((= bit 1) (right-branch cur-node))
+		  (else (error "bad bit ~0/1" bit))))
+
+(define (decode bits tree)
+	(define (decode-1 bits cur-node)
+		(if (null? bits)
+			'()
+			(let ((next-node (choose-branch (car bits) cur-node)))
+			   (if (leaf? next-node)
+				   (cons (symbol-leaf next-node)
+						 (decode-1 (cdr bits) tree))
+				   (decode-1 (cdr bits) next-node)))))
+	(decode-1 bits tree))
+
+
+(define (adjoin-set x set)
+	(cond ((null? set) (list x))
+		  ((< (weight x) (weight (car set))) (cons x set))
+		  (else (cons (car set) 
+				      (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+	(if (null? pairs)
+		'()
+		(let ((pair (car pairs)))
+			(adjoin-set (make-leaf (car pair)
+								   (cadr pair))
+			            (make-leaf-set (cdr pairs))))))
+
+
